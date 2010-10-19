615d820c159c87feb6190b71eb862b45fcadfb1f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2010-10-19 17:28:21 +0100

    added RUM stack version 1.0.0 from ATMEL
    this stack appears to be an updated version
    of what is in cpu/avr/radio/rf230, so I shall work on this then

---
+++ cpu/avr/radio/rum/dep/avr_timer.o.d
@@ -0,0 +1,101 @@
+avr_timer.o: avr_timer.c rum_types.h \
+ /usr/lib/gcc/avr/4.5.1/include/stdint.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdint.h mac_event.h \
+ mac.h stdbool.h /usr/lib/gcc/avr/4.5.1/include/stdbool.h hal.h hal_avr.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/io.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/sfr_defs.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/inttypes.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iom1281.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iomxx0_1.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/portpins.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/common.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/version.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/fuse.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/lock.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/interrupt.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/crc16.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay_basic.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/eeprom.h \
+ /usr/lib/gcc/avr/4.5.1/include/stddef.h at86rf23x_registermap.h serial.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdio.h \
+ /usr/lib/gcc/avr/4.5.1/include/stdarg.h sensors.h mac_route.h radio.h \
+ timer.h avr_timer.h system.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdlib.h sixlowpan_wake.h \
+ avr_sixlowpan.h
+
+rum_types.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stdint.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdint.h:
+
+mac_event.h:
+
+mac.h:
+
+stdbool.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stdbool.h:
+
+hal.h:
+
+hal_avr.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/io.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/sfr_defs.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iom1281.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iomxx0_1.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/portpins.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/common.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/version.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/fuse.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/lock.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/interrupt.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/crc16.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay_basic.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/eeprom.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stddef.h:
+
+at86rf23x_registermap.h:
+
+serial.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdio.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stdarg.h:
+
+sensors.h:
+
+mac_route.h:
+
+radio.h:
+
+timer.h:
+
+avr_timer.h:
+
+system.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdlib.h:
+
+sixlowpan_wake.h:
+
+avr_sixlowpan.h:
---
+++ cpu/avr/radio/rum/dep/sensors.o.d
@@ -0,0 +1,113 @@
+sensors.o: sensors.c radio.h /usr/lib/gcc/avr/4.5.1/include/stdint.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdint.h stdbool.h \
+ /usr/lib/gcc/avr/4.5.1/include/stdbool.h rum_types.h mac.h hal.h \
+ hal_avr.h /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/io.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/sfr_defs.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/inttypes.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iom1281.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iomxx0_1.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/portpins.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/common.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/version.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/fuse.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/lock.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/interrupt.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/crc16.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay_basic.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/eeprom.h \
+ /usr/lib/gcc/avr/4.5.1/include/stddef.h at86rf23x_registermap.h serial.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdio.h \
+ /usr/lib/gcc/avr/4.5.1/include/stdarg.h sensors.h mac_event.h \
+ mac_route.h mac_associate.h system.h timer.h avr_timer.h mac_data.h \
+ sleep.h /usr/lib/gcc/avr/4.5.1/../../../../avr/include/math.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdlib.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/string.h \
+ /usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/twi.h \
+ avr_sixlowpan.h
+
+radio.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stdint.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdint.h:
+
+stdbool.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stdbool.h:
+
+rum_types.h:
+
+mac.h:
+
+hal.h:
+
+hal_avr.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/io.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/sfr_defs.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iom1281.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/iomxx0_1.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/portpins.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/common.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/version.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/fuse.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/lock.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/interrupt.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/crc16.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/delay_basic.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/avr/eeprom.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stddef.h:
+
+at86rf23x_registermap.h:
+
+serial.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdio.h:
+
+/usr/lib/gcc/avr/4.5.1/include/stdarg.h:
+
+sensors.h:
+
+mac_event.h:
+
+mac_route.h:
+
+mac_associate.h:
+
+system.h:
+
+timer.h:
+
+avr_timer.h:
+
+mac_data.h:
+
+sleep.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/math.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/stdlib.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/string.h:
+
+/usr/lib/gcc/avr/4.5.1/../../../../avr/include/util/twi.h:
+
+avr_sixlowpan.h:
