96531d153538ee904e439198fba389e1eb8482cd
Ilya Dmitrichenko <errordeveloper@gmail.com>
2010-10-22 15:53:11 +0100

    working on radio.h

---
+++ cpu/avr/radio/rf2xx/vim/SESSION:radio.h
@@ -0,0 +1,519 @@
+let SessionLoad = 1
+if &cp | set nocp | endif
+let s:cpo_save=&cpo
+set cpo&vim
+inoremap <silent> <S-Tab> =BackwardsSnippet()
+imap <F10> =strftime("%c")
+imap <F5> :II
+imap <F4> :IIW
+imap <F3> :CheckSyntax
+imap <F2> :make! %
+imap <F1> 
+snoremap <silent> 	 i<Right>=TriggerSnippet()
+vmap  :call Toggle()
+nmap  :call Toggle()
+snoremap  b<BS>
+map  a
+snoremap % b<BS>%
+snoremap ' b<BS>'
+map Q gq
+snoremap U b<BS>U
+snoremap \ b<BS>\
+nmap \sv <Plug>SVNVimDiff
+nmap \su <Plug>SVNUpdate
+nmap \si <Plug>SVNInfo
+nmap \ss <Plug>SVNStatus
+nmap \sr <Plug>SVNReview
+nmap \sq <Plug>SVNRevert
+nmap \sl <Plug>SVNLog
+nmap \sg <Plug>SVNGotoOriginal
+nmap \sd <Plug>SVNDiff
+nmap \sc <Plug>SVNCommit
+nmap \sG <Plug>SVNClearAndGotoOriginal
+nmap \sn <Plug>SVNAnnotate
+nmap \sa <Plug>SVNAdd
+nmap \cwr <Plug>CVSWatchRemove
+nmap \cwf <Plug>CVSWatchOff
+nmap \cwn <Plug>CVSWatchOn
+nmap \cwa <Plug>CVSWatchAdd
+nmap \cwv <Plug>CVSWatchers
+nmap \cv <Plug>CVSVimDiff
+nmap \cu <Plug>CVSUpdate
+nmap \ct <Plug>CVSUnedit
+nmap \cs <Plug>CVSStatus
+nmap \cr <Plug>CVSReview
+nmap \cq <Plug>CVSRevert
+nmap \cl <Plug>CVSLog
+nmap \cg <Plug>CVSGotoOriginal
+nmap \ci <Plug>CVSEditors
+nmap \ce <Plug>CVSEdit
+nmap \cd <Plug>CVSDiff
+nmap \cc <Plug>CVSCommit
+nmap \cG <Plug>CVSClearAndGotoOriginal
+nmap \cn <Plug>CVSAnnotate
+nmap \ca <Plug>CVSAdd
+map \rwp <Plug>RestoreWinPosn
+map \swp <Plug>SaveWinPosn
+vmap <silent> \x <Plug>VisualTraditional
+vmap <silent> \c <Plug>VisualTraditionalj
+nmap <silent> \x <Plug>Traditional
+nmap <silent> \c <Plug>Traditionalj
+vmap <silent> \Htd :<BS><BS><BS>ma'>\Htd
+vmap <silent> \tt :<BS><BS><BS>ma'>\tt
+vmap <silent> \tp@ :<BS><BS><BS>ma'>\tp@
+vmap <silent> \tsq :<BS><BS><BS>ma'>\tsq
+vmap <silent> \tsp :<BS><BS><BS>ma'>\tsp
+vmap <silent> \tml :<BS><BS><BS>ma'>\tml
+vmap <silent> \tab :<BS><BS><BS>ma'>\tab
+vmap <silent> \t@ :<BS><BS><BS>ma'>\t@
+vmap <silent> \t? :<BS><BS><BS>ma'>\t?
+vmap <silent> \t= :<BS><BS><BS>ma'>\t=
+vmap <silent> \t< :<BS><BS><BS>ma'>\t<
+vmap <silent> \t; :<BS><BS><BS>ma'>\t;
+vmap <silent> \t: :<BS><BS><BS>ma'>\t:
+vmap <silent> \ts, :<BS><BS><BS>ma'>\ts,
+vmap <silent> \t, :<BS><BS><BS>ma'>\t,
+vmap <silent> \t| :<BS><BS><BS>ma'>\t|
+vmap <silent> \anum :B s/\(\d\)\s\+\(-\=[.,]\=\d\)/\1@\2/ge:AlignCtrl mp0P0gv:Align [.,@]:'<,'>s/\([-0-9.,]*\)\(\s\+\)\([.,]\)/\2\1\3/ge:'<,'>s/@/ /ge
+vmap <silent> \afnc :<BS><BS><BS>ma'>\afnc
+vmap <silent> \adef :<BS><BS><BS>ma'>\adef
+vmap <silent> \adec :<BS><BS><BS>ma'>\adec
+vmap <silent> \ascom :<BS><BS><BS>ma'>\ascom
+vmap <silent> \aocom :<BS><BS><BS>ma'>\aocom
+vmap <silent> \acom :<BS><BS><BS>ma'>\acom
+vmap <silent> \abox :<BS><BS><BS>ma'>\abox
+vmap <silent> \a= :<BS><BS><BS>ma'>\a=
+vmap <silent> \a< :<BS><BS><BS>ma'>\a<
+vmap <silent> \a, :<BS><BS><BS>ma'>\a,
+vmap <silent> \a? :<BS><BS><BS>ma'>\a?
+vmap <silent> \Tsp :<BS><BS><BS>ma'>\Tsp
+vmap <silent> \T@ :<BS><BS><BS>ma'>\T@
+vmap <silent> \T= :<BS><BS><BS>ma'>\T=
+vmap <silent> \T< :<BS><BS><BS>ma'>\T<
+vmap <silent> \T: :<BS><BS><BS>ma'>\T:
+vmap <silent> \Ts, :<BS><BS><BS>ma'>\Ts,
+vmap <silent> \T, :<BS><BS><BS>ma'>\T,
+vmap <silent> \T| :<BS><BS><BS>ma'>\T|
+map <silent> \tdW@ :AlignCtrl v ^\s*/[/*]:AlignCtrl mWp1P1=l @:'a,.Align
+map <silent> \tW@ :AlignCtrl mWp1P1=l @:'a,.Align
+nmap <silent> \t@ :AlignCtrl mIp1P1=l @:'a,.Align
+omap <silent> \t@ :AlignCtrl mIp1P1=l @:'a,.Align
+nmap <silent> \aocom :AlignPush:AlignCtrl g /[*/]\acom:AlignPop
+omap <silent> \aocom :AlignPush:AlignCtrl g /[*/]\acom:AlignPop
+nmap \rx <Plug>Rx
+nmap \ri <Plug>Ri
+snoremap ^ b<BS>^
+snoremap ` b<BS>`
+nmap gx <Plug>NetrwBrowseX
+snoremap <Left> bi
+snoremap <Right> a
+snoremap <BS> b<BS>
+snoremap <silent> <S-Tab> i<Right>=BackwardsSnippet()
+nnoremap <silent> <Plug>NetrwBrowseX :call netrw#NetrwBrowseX(expand("<cWORD>"),0)
+nnoremap <silent> <Plug>SVNCommitDiff :SVNCommitDiff
+nnoremap <silent> <Plug>SVNVimDiff :SVNVimDiff
+nnoremap <silent> <Plug>SVNUpdate :SVNUpdate
+nnoremap <silent> <Plug>SVNUnedit :SVNUnedit
+nnoremap <silent> <Plug>SVNInfo :SVNInfo
+nnoremap <silent> <Plug>SVNStatus :SVNStatus
+nnoremap <silent> <Plug>SVNReview :SVNReview
+nnoremap <silent> <Plug>SVNRevert :SVNRevert
+nnoremap <silent> <Plug>SVNLog :SVNLog
+nnoremap <silent> <Plug>SVNClearAndGotoOriginal :SVNGotoOriginal!
+nnoremap <silent> <Plug>SVNGotoOriginal :SVNGotoOriginal
+nnoremap <silent> <Plug>SVNDiff :SVNDiff
+nnoremap <silent> <Plug>SVNCommit :SVNCommit
+nnoremap <silent> <Plug>SVNAnnotate :SVNAnnotate
+nnoremap <silent> <Plug>SVNAdd :SVNAdd
+nnoremap <silent> <Plug>CVSWatchRemove :CVSWatchRemove
+nnoremap <silent> <Plug>CVSWatchOff :CVSWatchOff
+nnoremap <silent> <Plug>CVSWatchOn :CVSWatchOn
+nnoremap <silent> <Plug>CVSWatchAdd :CVSWatchAdd
+nnoremap <silent> <Plug>CVSWatchers :CVSWatchers
+nnoremap <silent> <Plug>CVSVimDiff :CVSVimDiff
+nnoremap <silent> <Plug>CVSUpdate :CVSUpdate
+nnoremap <silent> <Plug>CVSUnedit :CVSUnedit
+nnoremap <silent> <Plug>CVSStatus :CVSStatus
+nnoremap <silent> <Plug>CVSReview :CVSReview
+nnoremap <silent> <Plug>CVSRevert :CVSRevert
+nnoremap <silent> <Plug>CVSLog :CVSLog
+nnoremap <silent> <Plug>CVSClearAndGotoOriginal :CVSGotoOriginal!
+nnoremap <silent> <Plug>CVSGotoOriginal :CVSGotoOriginal
+nnoremap <silent> <Plug>CVSEditors :CVSEditors
+nnoremap <silent> <Plug>CVSEdit :CVSEdit
+nnoremap <silent> <Plug>CVSDiff :CVSDiff
+nnoremap <silent> <Plug>CVSCommit :CVSCommit
+nnoremap <silent> <Plug>CVSAnnotate :CVSAnnotate
+nnoremap <silent> <Plug>CVSAdd :CVSAdd
+nmap <silent> <Plug>RestoreWinPosn :call RestoreWinPosn()
+nmap <silent> <Plug>SaveWinPosn :call SaveWinPosn()
+noremap <Plug>VisualFirstLine :call EnhancedCommentify('', 'first',				    line("'<"), line("'>"))
+noremap <Plug>VisualTraditional :call EnhancedCommentify('', 'guess',				    line("'<"), line("'>"))
+noremap <Plug>VisualDeComment :call EnhancedCommentify('', 'decomment',				    line("'<"), line("'>"))
+noremap <Plug>VisualComment :call EnhancedCommentify('', 'comment',				    line("'<"), line("'>"))
+noremap <Plug>FirstLine :call EnhancedCommentify('', 'first')
+noremap <Plug>Traditional :call EnhancedCommentify('', 'guess')
+noremap <Plug>DeComment :call EnhancedCommentify('', 'decomment')
+noremap <Plug>Comment :call EnhancedCommentify('', 'comment')
+nmap <SNR>37_WE <Plug>AlignMapsWrapperEnd
+nmap <SNR>37_WS <Plug>AlignMapsWrapperStart
+map <F10> A =strftime("%c")
+map <F5> :II
+map <F4> :IIW
+map <F3> :CheckSyntax
+map <F2> :make! %
+nnoremap <Plug>Rx :call Ri('', 1)
+nnoremap <Plug>Ri :call Ri('', 0)
+map <F1> 
+inoremap <silent> 	 =TriggerSnippet()
+inoremap <silent> 	 =ShowAvailableSnips()
+imap  :call Toggle()
+inoremap  =GetCloseTag()
+noremap Û :call Ri(expand('<cword>'), 1)
+noremap Ý :call Ri(expand('<cWORD>'), 0)
+noremap É :call Ri('', 1)
+noremap é :call Ri('', 0)
+imap <silent> \x <Plug>Traditional
+imap <silent> \c <Plug>Traditionalji
+let &cpo=s:cpo_save
+unlet s:cpo_save
+set autoindent
+set background=dark
+set backspace=2
+set fileencodings=ucs-bom,utf-8,default,latin1
+set helplang=en
+set history=50
+set hlsearch
+set nomodeline
+set mouse=a
+set ruler
+set suffixes=.bak,~,.o,.h,.info,.swp,.obj,.info,.aux,.log,.dvi,.bbl,.out,.o,.lo
+set viminfo=!,'20,\"500
+let s:so_save = &so | let s:siso_save = &siso | set so=0 siso=0
+let v:this_session=expand("<sfile>:p")
+silent only
+cd ~/uni/ct3p60n/code/contiki-2.x/cpu/avr/radio/rf2xx
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+set shortmess=aoO
+badd +1 radio.h
+badd +0 ../rum/radio.h
+args radio.h ../rum/radio.h
+edit radio.h
+set splitbelow splitright
+wincmd _ | wincmd |
+split
+1wincmd k
+wincmd w
+set nosplitbelow
+set nosplitright
+wincmd t
+set winheight=1 winwidth=1
+exe '1resize ' . ((&lines * 34 + 35) / 71)
+exe '2resize ' . ((&lines * 34 + 35) / 71)
+argglobal
+setlocal keymap=
+setlocal noarabic
+setlocal autoindent
+setlocal nobinary
+setlocal bufhidden=
+setlocal buflisted
+setlocal buftype=
+setlocal nocindent
+setlocal cinkeys=0{,0},0),:,0#,!^F,o,O,e
+setlocal cinoptions=
+setlocal cinwords=if,else,while,do,for,switch
+setlocal comments=sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,://
+setlocal commentstring=/*%s*/
+setlocal complete=.,w,b,u,t,i
+setlocal completefunc=
+setlocal nocopyindent
+setlocal nocursorcolumn
+setlocal nocursorline
+setlocal define=
+setlocal dictionary=
+setlocal nodiff
+setlocal equalprg=
+setlocal errorformat=
+setlocal noexpandtab
+if &filetype != 'cpp'
+setlocal filetype=cpp
+endif
+setlocal foldcolumn=0
+setlocal foldenable
+setlocal foldexpr=0
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldmarker={{{,}}}
+setlocal foldmethod=manual
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldtext=foldtext()
+setlocal formatexpr=
+setlocal formatoptions=croql
+setlocal formatlistpat=^\\s*\\d\\+[\\]:.)}\\t\ ]\\s*
+setlocal grepprg=
+setlocal iminsert=0
+setlocal imsearch=0
+setlocal include=
+setlocal includeexpr=
+setlocal indentexpr=
+setlocal indentkeys=0{,0},:,0#,!^F,o,O,e
+setlocal noinfercase
+setlocal iskeyword=@,48-57,_,192-255
+setlocal keywordprg=
+setlocal nolinebreak
+setlocal nolisp
+setlocal nolist
+setlocal makeprg=
+setlocal matchpairs=(:),{:},[:]
+setlocal nomodeline
+setlocal modifiable
+setlocal nrformats=octal,hex
+setlocal nonumber
+set numberwidth=3
+setlocal numberwidth=3
+setlocal omnifunc=ccomplete#Complete
+setlocal path=
+setlocal nopreserveindent
+setlocal nopreviewwindow
+setlocal quoteescape=\\
+setlocal noreadonly
+setlocal norightleft
+setlocal rightleftcmd=search
+setlocal noscrollbind
+setlocal shiftwidth=8
+setlocal noshortname
+setlocal nosmartindent
+setlocal softtabstop=0
+setlocal nospell
+setlocal spellcapcheck=[.?!]\\_[\\])'\"\	\ ]\\+
+setlocal spellfile=
+setlocal spelllang=en
+setlocal statusline=
+setlocal suffixesadd=
+setlocal swapfile
+setlocal synmaxcol=3000
+if &syntax != 'cpp'
+setlocal syntax=cpp
+endif
+setlocal tabstop=8
+setlocal tags=
+setlocal textwidth=0
+setlocal thesaurus=
+setlocal nowinfixheight
+setlocal nowinfixwidth
+setlocal wrap
+setlocal wrapmargin=0
+silent! normal! zE
+1,39fold
+51,52fold
+60,65fold
+73,74fold
+76,79fold
+81,83fold
+89,97fold
+100,106fold
+108,123fold
+127,136fold
+139,144fold
+145,149fold
+145,149fold
+152,164fold
+1
+normal zc
+51
+normal zc
+60
+normal zc
+73
+normal zc
+76
+normal zc
+81
+normal zc
+89
+normal zc
+100
+normal zc
+108
+normal zc
+127
+normal zc
+139
+normal zc
+145
+normal zo
+145
+normal zc
+145
+normal zo
+152
+normal zc
+let s:l = 222 - ((33 * winheight(0) + 17) / 34)
+if s:l < 1 | let s:l = 1 | endif
+exe s:l
+normal! zt
+222
+normal! 0
+wincmd w
+argglobal
+2argu
+setlocal keymap=
+setlocal noarabic
+setlocal autoindent
+setlocal nobinary
+setlocal bufhidden=
+setlocal buflisted
+setlocal buftype=
+setlocal nocindent
+setlocal cinkeys=0{,0},0),:,0#,!^F,o,O,e
+setlocal cinoptions=
+setlocal cinwords=if,else,while,do,for,switch
+setlocal comments=sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,://
+setlocal commentstring=/*%s*/
+setlocal complete=.,w,b,u,t,i
+setlocal completefunc=
+setlocal nocopyindent
+setlocal nocursorcolumn
+setlocal nocursorline
+setlocal define=
+setlocal dictionary=
+setlocal nodiff
+setlocal equalprg=
+setlocal errorformat=
+setlocal noexpandtab
+if &filetype != 'cpp'
+setlocal filetype=cpp
+endif
+setlocal foldcolumn=0
+setlocal foldenable
+setlocal foldexpr=0
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldmarker={{{,}}}
+setlocal foldmethod=manual
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldtext=foldtext()
+setlocal formatexpr=
+setlocal formatoptions=croql
+setlocal formatlistpat=^\\s*\\d\\+[\\]:.)}\\t\ ]\\s*
+setlocal grepprg=
+setlocal iminsert=0
+setlocal imsearch=0
+setlocal include=
+setlocal includeexpr=
+setlocal indentexpr=
+setlocal indentkeys=0{,0},:,0#,!^F,o,O,e
+setlocal noinfercase
+setlocal iskeyword=@,48-57,_,192-255
+setlocal keywordprg=
+setlocal nolinebreak
+setlocal nolisp
+setlocal nolist
+setlocal makeprg=
+setlocal matchpairs=(:),{:},[:]
+setlocal nomodeline
+setlocal modifiable
+setlocal nrformats=octal,hex
+setlocal nonumber
+set numberwidth=3
+setlocal numberwidth=3
+setlocal omnifunc=ccomplete#Complete
+setlocal path=
+setlocal nopreserveindent
+setlocal nopreviewwindow
+setlocal quoteescape=\\
+setlocal noreadonly
+setlocal norightleft
+setlocal rightleftcmd=search
+setlocal noscrollbind
+setlocal shiftwidth=8
+setlocal noshortname
+setlocal nosmartindent
+setlocal softtabstop=0
+setlocal nospell
+setlocal spellcapcheck=[.?!]\\_[\\])'\"\	\ ]\\+
+setlocal spellfile=
+setlocal spelllang=en
+setlocal statusline=
+setlocal suffixesadd=
+setlocal swapfile
+setlocal synmaxcol=3000
+if &syntax != 'cpp'
+setlocal syntax=cpp
+endif
+setlocal tabstop=8
+setlocal tags=
+setlocal textwidth=0
+setlocal thesaurus=
+setlocal nowinfixheight
+setlocal nowinfixwidth
+setlocal wrap
+setlocal wrapmargin=0
+silent! normal! zE
+1,31fold
+33,34fold
+41,45fold
+47,48fold
+50,53fold
+55,57fold
+60,69fold
+72,79fold
+81,96fold
+98,106fold
+109,115fold
+116,120fold
+122,135fold
+1
+normal zc
+33
+normal zc
+41
+normal zc
+47
+normal zc
+50
+normal zc
+55
+normal zc
+60
+normal zc
+72
+normal zc
+81
+normal zc
+98
+normal zc
+109
+normal zc
+116
+normal zc
+122
+normal zc
+let s:l = 183 - ((33 * winheight(0) + 17) / 34)
+if s:l < 1 | let s:l = 1 | endif
+exe s:l
+normal! zt
+183
+normal! 0
+wincmd w
+2wincmd w
+exe '1resize ' . ((&lines * 34 + 35) / 71)
+exe '2resize ' . ((&lines * 34 + 35) / 71)
+tabnext 1
+if exists('s:wipebuf')
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20 shortmess=filnxtToO
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if file_readable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &so = s:so_save | let &siso = s:siso_save
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
